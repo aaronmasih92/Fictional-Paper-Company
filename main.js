66d33511c169b54515ac616ec7028acc10fb16be
Aaron <aaron@ashers-mbp.lan>
2018-03-22 23:44:17 -0400

    fixed some js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ slide();
 
 var pic = document.getElementById('pic');
 
-var randoPics = ["http://payment.americanarborists.net/wp-content/uploads/2016/09/planting-trees.jpg","http://www.mishawakatreeservice.com/uploads/8/4/0/3/84031734/_9726028_orig.jpg","https://aztrees.org/resources/SiteAlbums/33219080/ACTCws_tree_planting_21.JPG","http://actrees.org/wp-content/uploads/2016/07/Landlife-cocoons.jpg"]
+var randoPics = ["http://payment.americanarborists.net/wp-content/uploads/2016/09/planting-trees.jpg","http://www.mishawakatreeservice.com/uploads/8/4/0/3/84031734/_9726028_orig.jpg","https://aztrees.org/resources/SiteAlbums/33219080/ACTCws_tree_planting_21.JPG","http://actrees.org/wp-content/uploads/2016/07/Landlife-cocoons.jpg", "https://tarihnedio.com/wp-content/uploads/2017/12/How-to-make-best-and-fertile-soil-medium-for-your-plants-585x390.jpg"]
 
 
 function randoPic() {
