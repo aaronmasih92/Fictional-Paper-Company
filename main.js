5151d3158a82edbde7f9c7fec193eb3ab2659dda
Aaron <aaron@ashers-mbp.lan>
2018-03-22 23:41:52 -0400

    added some JS to environmental promise section

--- main.js
+++ main.js
@@ -13,3 +13,14 @@ let slide = function changePic() {
   setTimeout(changePic, 8000); // Change image every 8 seconds
 }
 slide();
+
+var pic = document.getElementById('pic');
+
+var randoPics = ["http://payment.americanarborists.net/wp-content/uploads/2016/09/planting-trees.jpg","http://www.mishawakatreeservice.com/uploads/8/4/0/3/84031734/_9726028_orig.jpg","https://aztrees.org/resources/SiteAlbums/33219080/ACTCws_tree_planting_21.JPG","http://actrees.org/wp-content/uploads/2016/07/Landlife-cocoons.jpg"]
+
+
+function randoPic() {
+  pic.src = randoPics[Math.floor(Math.random()*randoPics.length)];
+}
+
+document.getElementById('btn').addEventListener('click', randoPic);
